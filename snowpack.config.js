1d1e1ab63e719c40ee69e7ce3db415555d4731ac
Shane O'Moore <shaneiadt@gmail.com>
2020-11-08 14:17:01 +0000

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/',
+  },
+  plugins: ['@snowpack/plugin-typescript'],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    installTypes: true,
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+console.log('picture-in-picture');
+
+export {};
