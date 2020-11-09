0ffc5127765b8bf43930b711be08dd6063f81692
Shane O'Moore <shaneiadt@gmail.com>
2020-11-09 15:52:01 +0000

    extend HTMlVideoElement for requestPictureInPicture method

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
-console.log('picture-in-picture');
+interface PictureInPictire extends HTMLVideoElement {
+    requestPictureInPicture(): Promise<void>;
+}
 
-export {};
+const videoEl: PictureInPictire | null = document.querySelector('#video');
+const button: HTMLButtonElement | null = document.querySelector('#btn');
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        if (videoEl) {
+            videoEl.srcObject = mediaStream;
+            videoEl.onloadedmetadata = () => {
+                videoEl.play();
+            };
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+selectMediaStream();
+
+button?.addEventListener('click', async () => {
+    button.disabled = true;
+
+    await videoEl?.requestPictureInPicture();
+
+    button.disabled = false;
+});
+
+export { };
